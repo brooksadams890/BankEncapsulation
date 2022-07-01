07065d2b3c8466c3e01089a728672b8f95d3a07f
Brooks Adams <aba1421@jagmail.southalabama.edu>
2022-06-30 23:26:43 -0500

    init commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to depoist?");
+            
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {account.GetBalance()}");
+        }
+    }
+}
